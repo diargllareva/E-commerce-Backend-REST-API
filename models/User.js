a9908b10965c3f0ce748dbb265fdbb8ca99d2937
DiarGllareva <diargllareva2@gmail.com>
2026-02-07 17:59:30 +0100

    Initial commit

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+mongoose.models = {};
+mongoose.modelSchemas = {};
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: String,
+            default: "user",
+        }
+    },
+    { timestamps: true }  // <-- This automatically creates createdAt & updatedAt
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
